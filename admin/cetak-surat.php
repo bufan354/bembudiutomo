4d2cedf14d1b7ab8d190bd8005642ddd6b2aa224
bufan354 <bufan354@gmail.com>
2026-05-01 14:14:22 +0700

    perbaikan table tujuan dan titimangsa

--- admin/cetak-surat.php
+++ admin/cetak-surat.php
@@ -281,12 +281,19 @@ $download_name = "SURAT $f_perihal $f_kode UNTUK $f_tujuan $f_tahun";
         <?php endif; ?>
 
         <!-- 2. META SURAT -->
+        <!-- 
+            Kolom ke-4 (kanan) memiliki lebar TETAP sebesar 45mm.
+            Lebar ini cukup untuk menampung teks titi mangsa (misal: "Majalengka, 01 Mei 2026").
+            Titi mangsa rata KANAN di dalam kolom tersebut.
+            Blok Yth/tujuan/Di Tempat juga berada di kolom ke-4 yang sama,
+            sehingga tepi kiri tujuan otomatis sejajar dengan tepi kiri titi mangsa.
+        -->
         <table class="meta-surat" style="width: 100%; border-collapse: collapse;">
             <tr>
                 <td class="col-label" style="width: 75px;">Nomor</td>
                 <td class="col-titik" style="width: 15px;">:</td>
                 <td style="vertical-align: top;"><?php echo htmlspecialchars($surat['nomor_surat']); ?></td>
-                <td style="width: 1%; white-space: nowrap; text-align: left; vertical-align: top;">
+                <td style="width: 45mm; white-space: nowrap; text-align: right; vertical-align: top;">
                     <?php echo htmlspecialchars($surat['tempat_tanggal']); ?>
                 </td>
             </tr>
@@ -306,7 +313,7 @@ $download_name = "SURAT $f_perihal $f_kode UNTUK $f_tujuan $f_tahun";
             <tr>
                 <td class="col-label" style="vertical-align: top;">Perihal</td>
                 <td class="col-titik" style="vertical-align: top;">:</td>
-                <td style="vertical-align: top; padding-right: 30px;">
+                <td style="vertical-align: top;">
                     <div style="font-weight: bold; text-decoration: underline; line-height: 1.4;">
                         <?php echo htmlspecialchars($surat['perihal']); ?>
                     </div>
@@ -315,7 +322,15 @@ $download_name = "SURAT $f_perihal $f_kode UNTUK $f_tujuan $f_tahun";
             </tr>
             <tr>
                 <td colspan="3"></td>
-                <td style="vertical-align: top; padding-top: 15px; white-space: nowrap;">
+                <!-- 
+                    Tujuan berada di kolom ke-4 yang SAMA dengan titi mangsa.
+                    white-space: normal  → teks bisa wrap (tidak memaksa kolom melebar)
+                    text-align: left     → rata kiri di dalam kolom
+                    vertical-align: top  → mulai dari atas
+                    width: 45mm          → lebar kolom sudah dikunci di baris pertama,
+                                          tujuan hanya mengikuti, tidak bisa memperlebar
+                -->
+                <td style="vertical-align: top; padding-top: 15px; white-space: normal; text-align: left;">
                     Yth,<br>
                     <b><?php echo nl2br(htmlspecialchars($surat['tujuan'])); ?></b><br>
                     Di Tempat
@@ -375,7 +390,8 @@ $download_name = "SURAT $f_perihal $f_kode UNTUK $f_tujuan $f_tahun";
 
             <?php
             // Paragraf Permohonan: generate dinamis dengan akhiran cerdas
-            $tujuan_baris_pertama = trim(explode("\n", $surat['tujuan'])[0]);
+            // Gabungkan semua baris tujuan dengan spasi agar tidak ada kata yang terlewat
+            $tujuan_baris_pertama = trim(str_replace(["\r\n", "\r", "\n"], ' ', $surat['tujuan']));
             $konteks_text         = trim($konten['konteks'] ?? '');
             
             // Tentukan "buntut" kalimat (suffix) secara pintar
